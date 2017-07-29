b32f9e83f47dae876b018381af753ec4ebf356fd
zqianuser <1162980831@qq.com>
2017-07-29 16:26:20 +0800

    aaa

--- js/order.js
+++ js/order.js
@@ -1,9 +1,9 @@
-	var c = new Cart(PRODUCT_HOST+CART);
-	console.log(c)
-	
+	new Cart(PRODUCT_HOST+CART);
+
+   
  $("")	
  var url =  PRODUCT_HOST+USER+"?status=add&token="+localStorage.getItem("token");
- var parm = {address_name:"",consignee:"",country:"",province:"",city:"",district:"",address:"",zip_code:"",mobile:"",email:"",tel:};
+ var parm = {address_name:" ",consignee:"",country:"",province:"",city:"",district:"",address:"",zip_code:"",mobile:"",email:"",tel:""};
     $.post(url,parm,function (result) {
        // document.write(result.message);
         console.log(result.data);
